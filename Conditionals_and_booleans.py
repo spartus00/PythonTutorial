91ed550ac44483d4cdb49a7e8fccfcf979733827
Matthew Franson <mfranson@gmail.com>
2018-11-02 10:10:50 -0400

    Add conditionals and booleans

---
+++ Conditionals_and_booleans.py
@@ -0,0 +1,11 @@
+#if ___ (conditional we want to check)
+if True:
+    print('Conditional was True')
+
+if False:
+    print('Conditional was True')
+
+#Usually there isn't this simple standard true or false
+language = 'Python'
+if language == 'Python':
+    print('This is Python')
